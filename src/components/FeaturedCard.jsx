4282f5a806b8865e3da84d523c85068370df14b8
Josh Hundley <joshhundley@Joshs-MacBook-Pro.local>
2022-11-12 03:56:06 -0500

    Scrolling

--- src/components/FeaturedCard.jsx
+++ src/components/FeaturedCard.jsx
@@ -12,7 +12,7 @@ const FeaturedCard = ({place, cityName, index, subCollection}) => {
        {/* <a className='soe' href={"/things/"+cityName+"/"+place.name+place.address}>{place.name} is one of the best things in {cityName} Learn more Here {place.description}</a> */}
             <Link to={"/things/"+cityName+"/"+place.name+"?"+datString}>
             <img src={place.photo ? place.photo.images.large.url : assets.capeCoral1} alt="Photo Goes Here"/>
-            <h1 className='title'>{place.name}</h1>
+            <h2 className='title'>{place.name}</h2>
             <div className='footer'> </div>
             </Link>
         </div>
